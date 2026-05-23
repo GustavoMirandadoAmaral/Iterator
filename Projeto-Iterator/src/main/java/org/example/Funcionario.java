e7821a4801daed5e04cc1a158f176fb290ab6090
Gustavo Miranda do Amaral <gustavomirandadoamaral@gmail.com>
2026-05-23 16:57:20 -0300

    Implementação padrão Iterator

--- Projeto-Iterator/src/main/java/org/example/Funcionario.java
+++ Projeto-Iterator/src/main/java/org/example/Funcionario.java
@@ -10,22 +10,6 @@ public class Funcionario {
         this.ferias = ferias;
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public boolean isFerias() {
-        return ferias;
-    }
-
-    public void setFerias(boolean ferias) {
-        this.ferias = ferias;
-    }
-
     public boolean estaDeFerias() {
         return ferias;
     }
